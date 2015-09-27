e8fd7a19c2212559efc25ca1d36fc32e79b81a22
Vytautas Jakutis <jakutis@wix.com>
2015-09-27 20:44:55 +0300

    plan: 2. Code that sends a hello world string via usb every second

--- a.c
+++ a.c
@@ -1,20 +1,21 @@
-#include <avr/io.h>
+#include <stdio.h>
 #include <util/delay.h>
- 
-#define BLINK_DELAY_MS 50
- 
-int main (void)
-{
- /* set pin 5 of PORTB for output*/
- DDRB |= _BV(DDB5);
- 
- while(1) {
-  /* set pin 5 high to turn led on */
-  PORTB |= _BV(PORTB5);
-  _delay_ms(BLINK_DELAY_MS);
- 
-  /* set pin 5 low to turn led off */
-  PORTB &= ~_BV(PORTB5);
-  _delay_ms(BLINK_DELAY_MS);
- }
+
+#include "uart.h"
+
+#define PAUSE_DELAY_MS 1000
+
+int main(void) {    
+
+    uart_init();
+    stdout = &uart_output;
+    stdin  = &uart_input;
+                
+    while (1) {
+        if (puts("Hello world!") == 0) {
+            break;
+        }
+        _delay_ms(PAUSE_DELAY_MS);
+    }
+    return 0;
 }
--- make
+++ make
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o a.o a.c || exit 1
-avr-gcc -mmcu=atmega328p a.o -o a || exit 1
+COMPILE="avr-gcc -pedantic -Werror -std=c11 -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o"
+
+$COMPILE a.o a.c || exit 1
+$COMPILE uart.o uart.c || exit 1
+avr-gcc -mmcu=atmega328p uart.o a.o -o a || exit 1
 avr-objcopy -O ihex -R .eeprom a a.hex || exit 1
 avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:a.hex || exit 1
