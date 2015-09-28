8521bbcac4612d620962aae32e51b47d8ccc2fba
Vytautas Jakutis <jakutis@wix.com>
2015-09-28 08:32:24 +0300

    make: rename to make.sh, read device filename from file

--- make.sh
+++ make.sh
@@ -6,4 +6,4 @@ $COMPILE a.o a.c || exit 1
 $COMPILE uart.o uart.c || exit 1
 avr-gcc -mmcu=atmega328p uart.o a.o -o a || exit 1
 avr-objcopy -O ihex -R .eeprom a a.hex || exit 1
-avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:a.hex || exit 1
+sudo avrdude -F -V -c arduino -p ATMEGA328P -P "$(cat device)" -b 115200 -U flash:w:a.hex || exit 1
