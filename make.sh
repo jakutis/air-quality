f236dfeeabd4f4ed872747ddf2444ba4cc0eb621
Vytautas Jakutis <vytautas@jakut.is>
2015-11-12 19:41:21 +0200

    compile into build directory

--- make.sh
+++ make.sh
@@ -8,9 +8,10 @@ fi
 
 COMPILE="avr-gcc -pedantic -Werror -std=c11 -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o"
 
+mkdir -p build
 sudo apt-get install gcc-avr avr-libc avrdude || exit 1
-$COMPILE a.o a.c || exit 1
-$COMPILE uart.o uart.c || exit 1
-avr-gcc -mmcu=atmega328p uart.o a.o -o a || exit 1
-avr-objcopy -O ihex -R .eeprom a a.hex || exit 1
-sudo avrdude -F -V -c arduino -p ATMEGA328P -P "$DEVICE" -b 115200 -U flash:w:a.hex || exit 1
+$COMPILE build/a.o a.c || exit 1
+$COMPILE build/uart.o uart.c || exit 1
+avr-gcc -mmcu=atmega328p build/uart.o build/a.o -o build/a || exit 1
+avr-objcopy -O ihex -R .eeprom build/a build/a.hex || exit 1
+sudo avrdude -F -V -c arduino -p ATMEGA328P -P "$DEVICE" -b 115200 -U flash:w:build/a.hex || exit 1
